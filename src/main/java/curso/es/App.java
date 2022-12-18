128f394726c7b6fa01fecf99937af9ce7d0597d8
Ubuntu <ubuntu@ip-172-31-36-130.eu-west-1.compute.internal>
2022-12-18 20:03:34 +0000

    Alta

---
+++ src/main/java/curso/es/App.java
@@ -0,0 +1,19 @@
+package curso.es;
+
+public class App {
+ 
+    public static void main( String[] args ) {
+        System.out.println( "Hola manolo!" );
+        //System.out.println( App.sumar(1,5) ); // TRAZA 
+        
+    double SumandoError=App.sumar(2,4)  ;      
+    }
+
+    public static double sumar(double numero1 , double numero2) {
+        return numero1 + numero2;
+    }
+    
+    public static double Doblar(double NUMERO) {
+        return NUMERO*2;
+    }
+}
